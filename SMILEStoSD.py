976995d82749bc46baa156827275201b29a9d8a8
ting2025 <147067583+ting2025@users.noreply.github.com>
2024-03-14 08:45:23 +0800

    SMILEStoSD.py

--- SMILEStoSD.py
+++ SMILEStoSD.py
@@ -3,16 +3,13 @@ import csv
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
-# Define input and output paths
 input_folder = '/Users/tinaleung/Desktop/mechanism'
 csv_file = '/Users/tinaleung/Desktop/mechanism/mechanism.csv'
 output_folder = os.path.join(input_folder, 'mechanism_SD')
 
-# Create output folder if it doesn't exist
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-# Read CSV file and loop over rows
 with open(csv_file, 'r') as f:
     reader = csv.DictReader(f)
     for row in reader:
@@ -27,10 +24,7 @@ with open(csv_file, 'r') as f:
             print(f'Error: unable to generate molecule from SMILES "{smiles}" for image {image_name}')
             continue
 
-        # Kekulize the molecule
         Chem.Kekulize(mol)
-
-        # Generate SD file and write to output folder
         sd_file = os.path.join(output_folder, f'{image_name}.sdf')
         w = Chem.SDWriter(sd_file)
         w.write(mol)
